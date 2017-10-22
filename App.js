f2c715b5bfb8d64ec4fdece8b5e83c8ec0886b20
Jolan Wathelet <jolanwathelet@MBP-de-Jolan.lan>
2017-10-22 10:37:21 +0200

    fixing App.js

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import HomeScreen from './Components/Home';
+import TripsScreen from './Components/Trips'
+import ExpensesScreen from './Components/Expenses'
+
+const Router = StackNavigator({
+  Home: { screen: HomeScreen },
+  Trips: {screen: TripsScreen},
+  Expenses: {screen: ExpensesScreen}
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <Router />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
