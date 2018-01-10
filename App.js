e0bb4b5aecc2d4535982e7249660753d6889efa0
Jolan Wathelet <>
2018-01-10 14:55:37 +0100

    font modif 2

--- App.js
+++ App.js
@@ -30,6 +30,7 @@ export default class App extends React.Component {
 
         await Expo.Font.loadAsync({
             'Material Icons': require('./node_modules/react-native-vector-icons/Fonts/MaterialIcons.ttf'),
+            'MaterialIcons': require('./node_modules/react-native-vector-icons/Fonts/MaterialIcons.ttf'),
             'SimpleLineIcons': require('./node_modules/react-native-vector-icons/Fonts/SimpleLineIcons.ttf'),
             'FontAwesome': require('./node_modules/react-native-vector-icons/Fonts/FontAwesome.ttf'),
             'Ionicons': require('./node_modules/react-native-vector-icons/Fonts/Ionicons.ttf'),
